45d21110ddfab8f680d65e247b94897ebdec9bc5
franciscatsousa <francisca.tavares.s@gmail.com>
2024-05-28 12:58:19 +0100

    modal box no Calendar Button que aparece na homepage na 2º secção

--- js/index.js
+++ js/index.js
@@ -57,3 +57,24 @@ document.addEventListener("DOMContentLoaded", function() {
     prevButton.addEventListener('click', prevSlide);
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var alert = document.getElementById("warning");
+    var span = document.querySelector(".close");
+  
+    alert.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+
