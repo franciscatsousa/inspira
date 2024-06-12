94ee82603919176349b34d5123b93c6fd21bf98b
franciscatsousa <francisca.tavares.s@gmail.com>
2024-06-12 18:47:26 +0100

    warning is working on the second section of the homepage

--- js/index.js
+++ js/index.js
@@ -17,3 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     var modal = document.getElementById("myModal");
+//     var alert = document.getElementById("warning");
+//     var span = document.querySelector(".close");
+
+//     alert.onclick = function () {
+//         modal.style.display = "block";
+//         document.body.style.top = `-${window.scrollY}px`;
+//         document.body.style.position = 'fixed';
+//     }
+
+//     span.onclick = function () {
+//         modal.style.display = "none";
+//         document.body.style.position = '';
+//         document.body.style.top = '';
+//         const scrollY = document.body.style.top;
+//         document.body.style.position = '';
+//         document.body.style.top = '';
+//         window.scrollTo(0, parseInt(scrollY || '0' )* -1);
+//     }
+
+//     window.onclick = function (event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+// });
