09bb4afddc77aa67793562533d1917de9f09e6fb
franciscatsousa <francisca.tavares.s@gmail.com>
2024-07-16 22:58:18 +0100

    call-to-action section and warnings files changed

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ if (scrollArrow) {
     }
   });
 }
+
+function redirectToYouTube() {
+  if (confirm("You will be redirected to our Youtube channel")) {
+    window.open('https://www.youtube.com/channel/UCl_JZ0itrOvx-bkV0IajFZQ', '_blank'); // Open YouTube in a new tab
+  }
+}
+
+function redirectToInstagram() {
+  if (confirm("You will be redirected to our Instagram")) {
+    window.open('https://www.instagram.com/inspira_dei_uc/', '_blank'); // Open YouTube in a new tab
+  }
+}
+
--- js/warnings.js
+++ js/warnings.js
@@ -1,17 +1,19 @@
 const buttons = document.querySelectorAll('.warning-button');
 
-buttons.forEach((button) => {
-    const id = button.id?.split('warning')[1];
-    const modal = document.getElementById('modal' + id);
-    const closeBtn = document.getElementById('close' + id);
+function SignUpMentee() {
+    if (confirm("To sign up you will be redirected to a form on an external page.")) {
+      window.open('https://ls.uc.pt/index.php/298188?lang=pt', '_blank'); // Open YouTube in a new tab
+    }
+  }
 
-    button.onclick = function () {
-        modal.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    };
+  function SignUpMentor() {
+    if (confirm("To sign up you will be redirected to a form on an external page.")) {
+      window.open('https://ls.uc.pt/index.php/182886?lang=pt', '_blank'); // Open YouTube in a new tab
+    }
+  }
 
-    closeBtn.onclick = function () {
-        modal.style.display = 'none';
-        document.body.style.overflow = 'auto';
-    };
-});
+  function redirectToGuide() {
+  if (confirm("You will be redirected to an external page.")) {
+    window.open('https://drive.google.com/file/d/1YxwzJzT5PFGwXfgloOgqvHAbKwv5z4hP/view', '_blank'); // Open YouTube in a new tab
+  }
+}
